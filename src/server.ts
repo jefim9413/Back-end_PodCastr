38ed097cd120d026554075cc179fa2dfaf6c2eb7
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-06 14:36:52 -0300

    iniciando server do projeto

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log("🚀 Server Running 🚀"));
