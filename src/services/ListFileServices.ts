f8c03a2891c89f5ca74413bd7b395dab544cef09
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-06 15:24:56 -0300

    adicionando services da rota listar

---
+++ src/services/ListFileServices.ts
@@ -0,0 +1,12 @@
+import { getRepository } from "typeorm";
+import { File } from "../entities/file";
+
+class ListFileServices {
+  async execute() {
+    const repository = getRepository(File);
+    const listfile = await repository.find();
+    return listfile;
+  }
+}
+
+export { ListFileServices };
