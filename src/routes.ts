2c7bc5efb6e04f0fcf7b3225eb4507f94a269d67
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-06 15:23:36 -0300

    adicionando rota de listar files

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { CreateFileController } from "./controller/CreateFileController";
+import { ListFileController } from "./controller/ListFileController";
 
 const routes = Router();
-const fileController = new CreateFileController();
-routes.post("/file", fileController.handle);
+const createFileController = new CreateFileController();
+const listFileController = new ListFileController();
+routes.post("/file", createFileController.handle);
+routes.get("/file", listFileController.handle);
 
 export { routes };
