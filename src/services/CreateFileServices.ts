0b7bdb1486fe8b4c09fd93b07f42a675ac2631be
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-06 10:36:17 -0300

    finalizando services da entidade File

---
+++ src/services/CreateFileServices.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { File } from "../entities/file";
+
+type FileResquest = {
+  url: string;
+  type: string;
+  duration: number;
+};
+
+class CreateFileServices {
+  async execute({ url, duration, type }: FileResquest): Promise<File> {
+    const repository = getRepository(File);
+
+    const file = repository.create({
+      url,
+      duration,
+      type,
+    });
+
+    await repository.save(file);
+
+    return file;
+  }
+}
+
+export { CreateFileServices };
