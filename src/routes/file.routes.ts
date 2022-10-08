9387b4ca191d6759b0fa79a3bf80134e7d343af9
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-08 18:40:14 -0300

    adicionando a rota Update da entidade File

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { CreateFileController } from "../modules/files/controller/CreateFileController";
 import { ListFileController } from "../modules/files/controller/ListFileController";
 import { RemoveFileController } from "../modules/files/controller/RemoveFileController";
+import { UpdateFileController } from "../modules/files/controller/UpdateFileController";
 
 const fileRoutes = Router();
 const createFileController = new CreateFileController();
 const listFileController = new ListFileController();
 const removeFileController = new RemoveFileController();
+const updateFileController = new UpdateFileController();
 
 fileRoutes.post("/file", createFileController.handle);
 fileRoutes.get("/file", listFileController.handle);
 fileRoutes.delete("/file/:id", removeFileController.handle);
+fileRoutes.put("/file/:id", updateFileController.handle);
 
 export { fileRoutes };
