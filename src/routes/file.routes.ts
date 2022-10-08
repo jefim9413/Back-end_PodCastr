bd1cebda0230dca9e64f29788c8396030e4e618f
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-08 12:22:36 -0300

    adicionando rota Remover file

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateFileController } from "../modules/files/controller/CreateFileController";
 import { ListFileController } from "../modules/files/controller/ListFileController";
+import { RemoveFileController } from "../modules/files/controller/RemoveFileController";
 
 const fileRoutes = Router();
 const createFileController = new CreateFileController();
 const listFileController = new ListFileController();
+const removeFileController = new RemoveFileController();
 
 fileRoutes.post("/file", createFileController.handle);
 fileRoutes.get("/file", listFileController.handle);
+fileRoutes.delete("/file/:id", removeFileController.handle);
 
 export { fileRoutes };
