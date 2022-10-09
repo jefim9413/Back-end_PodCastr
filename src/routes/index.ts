b4255da6461a831db99616580712d8bfb3e95334
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-08 23:14:58 -0300

    adicionando rotas para entidade Episodes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { episodeRoutes } from "./episodes.routes";
 import { fileRoutes } from "./file.routes";
 
 const routes = Router();
 
 routes.use("/", fileRoutes);
+routes.use("/", episodeRoutes);
 
 export { routes };
